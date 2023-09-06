27ae742562f208a5ab66c72c96ce59f853246c8a
Adrian Guerrero <adrianson@outlook.com>
2023-09-06 16:01:29 -0600

    deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 conectarDB();
-const dominiosPermitidos = [process.env.FRONTEND_URL];
+const dominiosPermitidos = [process.env.VITE_FRONTEND_URL];
 
 const corsOptions = {
     origin: function(origin, callback){
